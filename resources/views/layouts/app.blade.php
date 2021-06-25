ef4acb380ec7bbc5dfb6f51bcbafd9662eed14c0
Eugene Yavgel <eugene.yavgel@gmail.com>
2021-06-25 21:15:08 +0300

    Welcome and Consultation pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,80 +34,35 @@
             </svg>
         </button>
         <ul class="c-header-nav ml-auto mr-4">
-            <li class="c-header-nav-item d-md-down-none mx-2"><a class="c-header-nav-link" href="#">
+            <li class="c-header-nav-item">
+                <a class="c-header-nav-link" href="{{ route('consultation') }}">{{ __('Get Consultation') }}</a>
+            </li>
+            <li class="c-header-nav-item d-md-down-none mx-2">
+                <a class="c-header-nav-link" href="{{ route('welcome') }}">
                     <svg class="c-icon">
-                        <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-men#cil-bell"></use>
+                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-settings"></use>
                     </svg>
-                </a></li>
-            <li class="c-header-nav-item d-md-down-none mx-2"><a class="c-header-nav-link" href="#">
+                </a>
+            </li>
+            <li class="c-header-nav-item dropdown">
+                <a class="c-header-nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                     <svg class="c-icon">
-                        <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-men#cil-list-rich"></use>
+                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-user"></use>
                     </svg>
-                </a></li>
-            <li class="c-header-nav-item d-md-down-none mx-2"><a class="c-header-nav-link" href="#">
-                    <svg class="c-icon">
-                        <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-men#cil-envelope-open"></use>
-                    </svg>
-                </a></li>
-            <li class="c-header-nav-item dropdown"><a class="c-header-nav-link" data-toggle="dropdown" href="#"
-                                                      role="button" aria-haspopup="true" aria-expanded="false">
-                    <div class="c-avatar">
-                        @if ( auth()->user()->email === 'eugene.yavgel@gmail.com' )
-                            <img class="c-avatar-img" src="/img/avatars/yavgel.png" alt="eugene.yavgel@gmail.com">
-                        @else
-                            <img class="c-avatar-img" src="/img/avatars/user.png" alt="user@email.com">
-                        @endif
-                    </div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right pt-0">
-                    <div class="dropdown-header bg-light py-2"><strong>Account</strong></div>
-                    <a class="dropdown-item" href="#">
-                        <svg class="c-icon mr-2">
-                            <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-men#cil-bell"></use>
-                        </svg>
-                        Updates<span class="badge badge-info ml-auto">42</span></a><a class="dropdown-item" href="#">
-                        <svg class="c-icon mr-2">
-                            <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-men#cil-envelope-open"></use>
-                        </svg>
-                        Messages<span class="badge badge-success ml-auto">42</span></a><a class="dropdown-item"
-                                                                                          href="#">
-                        <svg class="c-icon mr-2">
-                            <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-men#cil-task"></use>
-                        </svg>
-                        Tasks<span class="badge badge-danger ml-auto">42</span></a><a class="dropdown-item" href="#">
-                        <svg class="c-icon mr-2">
-                            <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-men#cil-comment-square"></use>
-                        </svg>
-                        Comments<span class="badge badge-warning ml-auto">42</span></a>
-                    <div class="dropdown-header bg-light py-2"><strong>Settings</strong></div>
-                    <a class="dropdown-item" href="#">
-                        <svg class="c-icon mr-2">
-                            <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-men#cil-user"></use>
-                        </svg>
-                        Profile</a><a class="dropdown-item" href="#">
-                        <svg class="c-icon mr-2">
-                            <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-men#cil-settings"></use>
-                        </svg>
-                        Settings</a><a class="dropdown-item" href="#">
-                        <svg class="c-icon mr-2">
-                            <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-men#cil-credit-card"></use>
-                        </svg>
-                        Payments<span class="badge badge-secondary ml-auto">42</span></a><a class="dropdown-item"
-                                                                                            href="#">
-                        <svg class="c-icon mr-2">
-                            <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-men#cil-file"></use>
-                        </svg>
-                        Projects<span class="badge badge-primary ml-auto">42</span></a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">
-                        <svg class="c-icon mr-2">
-                            <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-men#cil-lock-locked"></use>
-                        </svg>
-                        Lock Account</a><a class="dropdown-item" href="#">
-                        <svg class="c-icon mr-2">
-                            <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-men#cil-account-logout"></use>
-                        </svg>
-                        Logout</a>
+                    <div class="dropdown-header bg-light py-2">
+                        <strong>Account</strong>
+                    </div>
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                    >
+                    <svg class="c-icon mr-2">
+                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
+                    </svg> Logout</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                 </div>
             </li>
         </ul>
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::group(['middleware' => 'auth'], function () {
+    Route::get('welcome', [\App\Http\Controllers\PageController::class, 'welcome'])->name('welcome');
+    Route::get('consultation', [\App\Http\Controllers\PageController::class, 'consultation'])->name('consultation');
+
     Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'is_admin'], function () {
         Route::resource('pages', PageController::class)->only(['edit', 'update']);
         Route::resource('checklist_groups', ChecklistGroupController::class);
