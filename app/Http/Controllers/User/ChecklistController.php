e6dc1f1d00a84e04fcbdc93cde2c65c3bc0d25cb
Eugene Yavgel <eugene.yavgel@gmail.com>
2021-06-28 15:08:11 +0300

    User's checklists on the sidebar

---
+++ app/Http/Controllers/User/ChecklistController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Checklist;
+use Illuminate\Contracts\View\View;
+
+class ChecklistController extends Controller
+{
+    public function show(Checklist $checklist): View
+    {
+        return view('users.checklists.show', compact('checklist'));
+    }
+}
